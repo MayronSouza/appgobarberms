db4460a0965089a264507b12094c128e42385b8b
MayronSouza <mayron.souza@gmz.solutions>
2020-11-18 16:15:01 -0300

    Iniciando o desenvolvimento da page de criaçao de agendamento, configurando a tipagem do ID do prestador que foi enviado pela page Dashboard, criando, tambem, o Header da page

---
+++ src/pages/CreateAppointment/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  margin-left: 16px;
+
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  color: #f5ede8;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -70,7 +70,7 @@ const Dashboard: React.FC = () => {
         data={providers}
         keyExtractor={provider => provider.id}
         ListHeaderComponent={
-          <ProviderListTitle>Cabeleireiros</ProviderListTitle>
+          <ProviderListTitle>Cabeleireiros&Barbeiros</ProviderListTitle>
         }
         renderItem={({ item: provider }) => (
           <ProviderContainer
